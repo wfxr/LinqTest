3c21fd050550ef7f08414121cdc36a45c6a51469
wfxr <wfxrgm@gmail.com>
2015-01-21 21:44:10 +0800

    测试完成

---
+++ LinqTest/LinqTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LinqTest
+{
+    internal class LinqTest
+    {
+        public LinqTest(List<object> samples, object sought, int repeatTimes = 1)
+        {
+            Samples = samples;
+            Sought = sought;
+            RepeatTimes = repeatTimes;
+        }
+
+        private delegate void TestDelegate();
+
+        private Int64 TimingTest(TestDelegate TestMethod)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            for (var i = 0; i < RepeatTimes; ++i)
+                TestMethod();
+            sw.Stop();
+            return sw.ElapsedMilliseconds / RepeatTimes;
+        }
+
+        public Int64 DoLinqTimingTest() { return TimingTest(DoLinq); }
+        public Int64 DoForeachTimingTest() { return TimingTest(DoForeach); }
+
+        private void DoLinq()
+        {
+            var result = Samples.Where(item => Equals(item, Sought)).ToList();
+        }
+
+        private void DoForeach()
+        {
+            var results = new List<object>();
+            foreach (var item in Samples)
+                if (Equals(item, Sought))
+                    results.Add(item);
+        }
+
+        public int RepeatTimes
+        {
+            get { return _repeatTimes; }
+            set {
+                _repeatTimes = value < 1 ? 1 : value;
+            }
+        }
+
+        private List<object> Samples { get; set; }
+        private object Sought { get; set; }
+
+        private int _repeatTimes;
+    }
+}
---
+++ LinqTest/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace LinqTest {
+    internal class Program {
+        private static void Main(string[] args) {
+            const int minSampleSize = 5120000;
+            const int maxSampleSize = 128000000;
+            const int repeatTimes = 3;
+            const int step = maxSampleSize/4;
+            const int growthFactor = 2;
+            for (var sampleSize = step; sampleSize <= maxSampleSize;
+                sampleSize += step) {
+                Console.WriteLine("\n\n\tSample size: {0:N0}", sampleSize);
+
+                Console.WriteLine("\nTest using List<string>:");
+                TestUsingStringList(sampleSize, repeatTimes);
+
+                Console.WriteLine("\nTest using List<int>:");
+                TestUsingIntList(sampleSize, repeatTimes);
+
+                Console.WriteLine("\nTest using List<UserType>:");
+                TestUsingUserTypeList(sampleSize, repeatTimes);
+                Console.WriteLine("========================================");
+            }
+        }
+
+        private static void TestUsingStringList(int sampleSize, int repeatTimes) {
+            var strSamples = PrepareStringSamples(sampleSize);
+            DoTest(strSamples, repeatTimes);
+        }
+        private static void TestUsingIntList(int sampleSize, int repeatTimes) {
+            var intSamples = PrepareIntSamples(sampleSize);
+            DoTest(intSamples, repeatTimes);
+        }
+
+        private static void TestUsingUserTypeList(int sampleSize, int repeatTimes) {
+            var intSamples = PrepareUserTypeSamples(sampleSize);
+            DoTest(intSamples, repeatTimes);
+        }
+        private static void DoTest(List<object> samples, int repeatTimes)
+        {
+            var rand = new Random();
+            var sampleSize = samples.Count;
+            var sought = samples[rand.Next(0, sampleSize)];
+            var lt = new LinqTest(samples, sought, repeatTimes);
+
+            Console.WriteLine("operation\ttime");
+            Console.WriteLine("------------------------");
+
+            var msForeach = lt.DoForeachTimingTest();
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            ShowElapsedTime(msForeach, "foreach");
+            var msLinq = lt.DoLinqTimingTest();
+            Console.ForegroundColor = ConsoleColor.Green;
+            ShowElapsedTime(msLinq, "linq");
+            Console.ForegroundColor = defaultColor;
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("linq's relative efficiency: {0:P}",
+                (double)msForeach / msLinq);
+
+        }
+        private static void ShowElapsedTime(Int64 time, string name) {
+            Console.WriteLine("{0}\t\t{1}ms", name, time);
+        }
+
+        private static List<object> PrepareSamples(int sampleSize, List<object> baseSample) {
+            var samples = new List<object>();
+            for (var i = 0; i < sampleSize; ++i)
+                samples.Add(baseSample[i%baseSample.Count]);
+            return samples;
+        }
+
+        private static List<object> PrepareStringSamples(int sampleSize) {
+            var baseSample = new List<object>()
+            {"Tom", "Jack", "John", "Smith", "Richard"};
+            return PrepareSamples(sampleSize, baseSample);
+        }
+
+        private static List<object> PrepareIntSamples(int sampleSize) {
+            var baseSample = new List<object>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+            return PrepareSamples(sampleSize, baseSample);
+        }
+
+        private static List<object> PrepareUserTypeSamples(int sampleSize) {
+            var baseSample = new List<object>() {
+                new Student("Tom", "1043062001", 100), 
+                new Student("Jack", "1043062002", 87),
+                new Student("John", "1043062003", 96),
+                new Student("Smith", "1043062004", 52),
+                new Student("Richard", "1043062005", 78)
+            };
+            return PrepareSamples(sampleSize, baseSample);
+        }
+    }
+
+    class Student {
+        public Student(string name, string id, int score) {
+            Name = name;
+            Id = id;
+            Score = score;
+        }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Score { get; set; }
+    }
+}
